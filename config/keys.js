a8549c4c0d427bc51e0bf76f4978ec382427ed5a
annacho <annacho13@gmail.com>
2018-11-10 18:41:36 -0500

    Add JWT authentication and validation handlers

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-  'mongodb://alex:FpPik95qEMkpK79@ds157223.mlab.com:57223/devconnector1'
+  'mongodb://alex:FpPik95qEMkpK79@ds157223.mlab.com:57223/devconnector1',
+  secretOrKey: 'secret'
 };
