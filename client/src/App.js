c82f9e76e001422f986e9b0c949f20f77fa9e755
annacho <annacho13@gmail.com>
2018-11-18 22:35:29 -0500

    Add TextFieldGroupInput component and profile state

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from '../../actions/profileActions';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -19,7 +20,7 @@ import './App.css';
 if(localStorage.jwtToken) {
   // Set auth token header auth
   setAuthToken(localStorage.jwtToken);
-  // Decode tokne and get user info and exp
+  // Decode token and get user info and exp
   const decoded = jwt_decode(localStorage.jwtToken);
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
@@ -29,8 +30,8 @@ if(localStorage.jwtToken) {
   if (decoded.exp < currentTime) {
     // Logout user
     store.dispatch(logoutUser());
-    // TODO: Clear current Profile
-
+    // Clear current Profile
+    store.dispatch(clearCurrentProfile());
     // Redirect to login
     window.location.href = '/login';
   }
---
+++ client/src/components/dashboard/dashboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import PropTypes 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profileActions
+
+
+class Dashboard extends extends Component {
+  render () {
+    return (
+      <div>
+      dashboard
+      </div>
+    )
+  }
+}
+
+export default Dashboard;
