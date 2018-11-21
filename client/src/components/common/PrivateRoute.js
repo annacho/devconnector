69f6a5794825d3433fc5317db7151d2894edca0c
annacho <annacho13@gmail.com>
2018-11-20 19:38:20 -0500

    Add experience and education form and functionality

--- client/src/components/common/PrivateRoute.js
+++ client/src/components/common/PrivateRoute.js
@@ -3,17 +3,18 @@ import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-const PrivateRoute = ({component: Component, auth, ...rest }) => {
+const PrivateRoute = ({component: Component, auth, ...rest }) => (
   <Route
     {...rest}
     render = {props =>
       auth.isAuthenticated === true ? (
         <Component {...props} />
       ) : (
-        <Redirect to='/login' />
+        <Redirect to="/login" />
       )
     }
-};
+  />
+);
 
 PrivateRoute.propTypes = {
   auth: PropTypes.object.isRequired
