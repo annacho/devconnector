d81d5b9a073fda86818551702e57cb30bf1d6321
annacho <annacho13@gmail.com>
2018-11-25 14:26:44 -0500

    Add bug fix

--- client/src/components/post/CommentFeed.js
+++ client/src/components/post/CommentFeed.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import CommentItem from './post/CommentItem';
+import CommentItem from './CommentItem';
 
 class CommentFeed extends Component {
   render() {
